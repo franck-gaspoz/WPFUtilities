3df5f8be1368cac0cc6d6d74f5951a2e4c077466
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-03 08:56:19 +0100

    fix comments

--- WPFUtilities/Converters/StringToWindowStateConverter.cs
+++ WPFUtilities/Converters/StringToWindowStateConverter.cs
@@ -3,21 +3,24 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
+using WPFUtilities.ComponentModel;
+
 namespace WPFUtilities.Converters
 {
+    /// <summary>
+    /// string to window state converter
+    /// </summary>
     public class StringToWindowStateConverter
-        : IValueConverter
+        : Singleton<StringToWindowStateConverter>, IValueConverter
     {
-        static StringToWindowStateConverter _instance;
-        public static StringToWindowStateConverter Instance
-        {
-            get
-            {
-                _instance = _instance ?? new StringToWindowStateConverter();
-                return _instance;
-            }
-        }
-
+        /// <summary>
+        /// transforms a string value representing a window state to a window state value, returns Normal if fail, returns collapsed if not a string value
+        /// </summary>
+        /// <param name="value">value, expects a double</param>
+        /// <param name="targetType">target type</param>
+        /// <param name="parameter">parameter</param>
+        /// <param name="culture">culture</param>
+        /// <returns>transformed value or Collapsed if not a string value</returns>
         public object Convert(
             object value,
             Type targetType,
@@ -37,13 +40,16 @@ namespace WPFUtilities.Converters
             }
         }
 
-        public object ConvertBack(
-            object value,
-            Type targetType,
-            object parameter,
-            CultureInfo culture)
-        {
-            return value?.ToString();
-        }
+        /// <summary>
+        /// convert back
+        /// </summary>
+        /// <exception cref="NotImplementedException">not implemented</exception>
+        /// <param name="value">value</param>
+        /// <param name="targetType">target type</param>
+        /// <param name="parameter">parameter</param>
+        /// <param name="culture">culture</param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
     }
 }
