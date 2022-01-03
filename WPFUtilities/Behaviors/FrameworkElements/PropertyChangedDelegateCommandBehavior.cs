805f08cb7ab82496ed86176b21822bd14a1a5e40
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-03 07:35:16 +0100

    fix behavior

--- WPFUtilities/Behaviors/FrameworkElements/PropertyChangedDelegateCommandBehavior.cs
+++ WPFUtilities/Behaviors/FrameworkElements/PropertyChangedDelegateCommandBehavior.cs
@@ -7,25 +7,42 @@ using Microsoft.Xaml.Behaviors;
 
 namespace WPFUtilities.Behaviors.FrameworkElements
 {
+    /// <summary>
+    /// property changed command delegate behavior
+    /// </summary>
     public class PropertyChangedDelegateCommandBehavior
         : Behavior<FrameworkElement>
     {
         #region Command
 
+        /// <summary>
+        /// command
+        /// </summary>
         public ICommand Command
         {
             get => (ICommand)GetValue(CommandProperty);
             set { SetValue(CommandProperty, value); }
         }
 
-        public static ICommand GetCommand(DependencyObject obj)
-            => (ICommand)obj.GetValue(CommandProperty);
+        /// <summary>
+        /// get command
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>command</returns>
+        public static ICommand GetCommand(DependencyObject dependencyObject)
+            => (ICommand)dependencyObject.GetValue(CommandProperty);
 
-        public static void SetCommand(DependencyObject obj, ICommand value)
-        {
-            obj.SetValue(CommandProperty, value);
-        }
+        /// <summary>
+        /// set command
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value">value</param>
+        public static void SetCommand(DependencyObject dependencyObject, ICommand value)
+            => dependencyObject.SetValue(CommandProperty, value);
 
+        /// <summary>
+        /// command property
+        /// </summary>
         public static readonly DependencyProperty CommandProperty =
             DependencyProperty.RegisterAttached("Command", typeof(ICommand), typeof(PropertyChangedDelegateCommandBehavior), new PropertyMetadata(null));
 
@@ -33,20 +50,34 @@ namespace WPFUtilities.Behaviors.FrameworkElements
 
         #region CommandParameter
 
+        /// <summary>
+        /// command parameter
+        /// </summary>
         public object CommandParameter
         {
             get => (object)GetValue(CommandParameterProperty);
             set { SetValue(CommandParameterProperty, value); }
         }
 
-        public static object GetCommandParameter(DependencyObject obj)
-            => (object)obj.GetValue(CommandParameterProperty);
+        /// <summary>
+        /// get command
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>command</returns>
+        public static object GetCommandParameter(DependencyObject dependencyObject)
+            => (object)dependencyObject.GetValue(CommandParameterProperty);
 
-        public static void SetCommandParameter(DependencyObject obj, object value)
-        {
-            obj.SetValue(CommandParameterProperty, value);
-        }
+        /// <summary>
+        /// set command
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value"></param>
+        public static void SetCommandParameter(DependencyObject dependencyObject, object value)
+            => dependencyObject.SetValue(CommandParameterProperty, value);
 
+        /// <summary>
+        /// command parameter
+        /// </summary>
         public static readonly DependencyProperty CommandParameterProperty =
             DependencyProperty.RegisterAttached("CommandParameter", typeof(object), typeof(PropertyChangedDelegateCommandBehavior), new PropertyMetadata(null));
 
@@ -54,20 +85,34 @@ namespace WPFUtilities.Behaviors.FrameworkElements
 
         #region Source
 
+        /// <summary>
+        /// source
+        /// </summary>
         public INotifyPropertyChanged Source
         {
             get => (INotifyPropertyChanged)GetValue(SourceProperty);
             set { SetValue(SourceProperty, value); }
         }
 
-        public static INotifyPropertyChanged GetSource(DependencyObject obj)
-            => (INotifyPropertyChanged)obj.GetValue(SourceProperty);
+        /// <summary>
+        /// get source
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>source</returns>
+        public static INotifyPropertyChanged GetSource(DependencyObject dependencyObject)
+            => (INotifyPropertyChanged)dependencyObject.GetValue(SourceProperty);
 
-        public static void SetSource(DependencyObject obj, INotifyPropertyChanged value)
-        {
-            obj.SetValue(SourceProperty, value);
-        }
+        /// <summary>
+        /// set source
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value">value</param>
+        public static void SetSource(DependencyObject dependencyObject, INotifyPropertyChanged value)
+            => dependencyObject.SetValue(SourceProperty, value);
 
+        /// <summary>
+        /// source property
+        /// </summary>
         public static readonly DependencyProperty SourceProperty =
             DependencyProperty.RegisterAttached("Source", typeof(INotifyPropertyChanged), typeof(PropertyChangedDelegateCommandBehavior), new PropertyMetadata(null));
 
@@ -75,25 +120,40 @@ namespace WPFUtilities.Behaviors.FrameworkElements
 
         #region PropertyName
 
+        /// <summary>
+        /// property name
+        /// </summary>
         public string PropertyName
         {
             get => (string)GetValue(PropertyNameProperty);
             set { SetValue(PropertyNameProperty, value); }
         }
 
-        public static string GetPropertyName(DependencyObject obj)
-            => (string)obj.GetValue(PropertyNameProperty);
+        /// <summary>
+        /// get property name
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>property name</returns>
+        public static string GetPropertyName(DependencyObject dependencyObject)
+            => (string)dependencyObject.GetValue(PropertyNameProperty);
 
-        public static void SetPropertyName(DependencyObject obj, string value)
-        {
-            obj.SetValue(PropertyNameProperty, value);
-        }
+        /// <summary>
+        /// set property name
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value">value</param>
+        public static void SetPropertyName(DependencyObject dependencyObject, string value)
+            => dependencyObject.SetValue(PropertyNameProperty, value);
 
+        /// <summary>
+        /// property name dependency property
+        /// </summary>
         public static readonly DependencyProperty PropertyNameProperty =
             DependencyProperty.RegisterAttached("PropertyName", typeof(string), typeof(PropertyChangedDelegateCommandBehavior), new PropertyMetadata(null));
 
         #endregion
 
+        /// <inheritdoc/>
         protected override void OnAttached()
         {
             var source = Source ?? throw new ArgumentNullException(nameof(Source));
@@ -116,6 +176,7 @@ namespace WPFUtilities.Behaviors.FrameworkElements
             }
         }
 
+        /// <inheritdoc/>
         protected override void OnDetaching()
         {
             Source.PropertyChanged -= Source_PropertyChanged;
