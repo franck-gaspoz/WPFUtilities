c67c694eea211540a351c192b146a93f2a829f0e
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-03 08:35:04 +0100

    fix comments

--- WPFUtilities/Converters/DebugConverter.cs
+++ WPFUtilities/Converters/DebugConverter.cs
@@ -2,45 +2,61 @@
 using System.Globalization;
 using System.Windows.Data;
 
+using WPFUtilities.ComponentModel;
+
 namespace WPFUtilities.Converters
 {
+    /// <summary>
+    /// multi-purpose debug converter
+    /// </summary>
     public class DebugConverter
-        : IValueConverter
+        : Singleton<DebugConverter>, IValueConverter
     {
-        static int n = 0;
+        static int _instanceCount = 0;
 
-        static DebugConverter _instance;
-        public static DebugConverter Instance
-            => _instance ?? (_instance = new DebugConverter());
+        static Action<DebugConverter> CallBack;
 
-        public static DebugConverter GetInstance()
+        /// <summary>
+        /// build a new instance, update instance counter
+        /// </summary>
+        public DebugConverter()
         {
-            return Instance;
+            _instanceCount++;
         }
 
-        protected DebugConverter()
-        {
-            //System.Diagnostics.Debug.WriteLine($"DebugConvert #{n}");
-            n++;
-        }
-
+        /// <summary>
+        /// convert
+        /// </summary>
+        /// <param name="value">value</param>
+        /// <param name="targetType">target ype</param>
+        /// <param name="parameter">parameter</param>
+        /// <param name="culture">culture</param>
+        /// <returns>value</returns>
         public object Convert(
             object value,
             Type targetType,
             object parameter,
             CultureInfo culture)
         {
-            //Debugger.Break();
+            CallBack?.Invoke(this);
             return value;
         }
 
+        /// <summary>
+        /// convert back value
+        /// </summary>
+        /// <param name="value">value</param>
+        /// <param name="targetType">target type</param>
+        /// <param name="parameter">parameter</param>
+        /// <param name="culture">culture</param>
+        /// <returns>value</returns>
         public object ConvertBack(
             object value,
             Type targetType,
             object parameter,
             CultureInfo culture)
         {
-            //Debugger.Break();
+            CallBack?.Invoke(this);
             return value;
         }
     }
