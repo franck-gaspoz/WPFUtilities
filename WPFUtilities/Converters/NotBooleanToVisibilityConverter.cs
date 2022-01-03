44bb189515a768973f40e5e8f468c9369235e5df
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-03 08:41:39 +0100

    fix comments

--- WPFUtilities/Converters/NotBooleanToVisibilityConverter.cs
+++ WPFUtilities/Converters/NotBooleanToVisibilityConverter.cs
@@ -3,15 +3,25 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
+using WPFUtilities.ComponentModel;
+
 namespace WPFUtilities.Converters
 {
-    public class NotBooleanToVisibilityConverter
-        : IValueConverter
+    /// <summary>
+    /// converts a boolean to a visibility value
+    /// </summary>
+    public class NotBooleanToVisibilityConverter :
+        Singleton<NotBooleanToVisibilityConverter>,
+        IValueConverter
     {
-        static NotBooleanToVisibilityConverter _instance;
-        public static NotBooleanToVisibilityConverter Instance
-            => _instance = _instance ?? (_instance = new NotBooleanToVisibilityConverter());
-
+        /// <summary>
+        /// convert from bool to Visibility
+        /// </summary>
+        /// <param name="value">value, expects a boolean</param>
+        /// <param name="targetType">target type</param>
+        /// <param name="parameter">parameter</param>
+        /// <param name="culture">culture</param>
+        /// <returns>Visiblity.Visibile if value is false, Collapsed otherwize returns Collapsed if value is not a Visibility</returns>
         public object Convert(
             object value,
             Type targetType,
@@ -20,8 +30,8 @@ namespace WPFUtilities.Converters
         {
             try
             {
-                bool v = (bool)value;
-                return !v ?
+                var visibility = !(bool)value;
+                return visibility ?
                     Visibility.Visible :
                     ((parameter is Visibility notVisibleVisibility) ?
                         notVisibleVisibility :
@@ -33,13 +43,29 @@ namespace WPFUtilities.Converters
             }
         }
 
+        /// <summary>
+        /// convert back from Visiblity to bool
+        /// </summary>
+        /// <param name="value">value, expects a visiblity</param>
+        /// <param name="targetType">expect </param>
+        /// <param name="parameter">parameter</param>
+        /// <param name="culture">culture</param>
+        /// <returns>false if visivility is Visible, true otherwize returns true if value is not a boolean</returns>
         public object ConvertBack(
             object value,
             Type targetType,
             object parameter,
             CultureInfo culture)
         {
-            return null;
+            try
+            {
+                var visibility = (Visibility)value;
+                return visibility == Visibility.Visible ? false : true;
+            }
+            catch
+            {
+                return true;
+            }
         }
     }
 }
