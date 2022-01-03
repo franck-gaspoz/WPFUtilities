483b389801263f6b2fbdf1c4d2a42c0a5a2d84c4
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-03 08:00:23 +0100

    fix commnents

--- WPFUtilities/Behaviors/ScrollViewers/ScrollViewerHelperFeatureBehavior.cs
+++ WPFUtilities/Behaviors/ScrollViewers/ScrollViewerHelperFeatureBehavior.cs
@@ -5,51 +5,73 @@ using Microsoft.Xaml.Behaviors;
 
 namespace WPFUtilities.Behaviors.ScrollViewers
 {
+    /// <summary>
+    /// scroll viewer helper feature behavior. exports scroll viewer properties to a model
+    /// </summary>
     public class ScrollViewerHelperFeatureBehavior :
         Behavior<ScrollViewer>
     {
+        #region scroll viewer helper feature
+
+        /// <summary>
+        /// scroll viewer helper feature model
+        /// </summary>
         public IScrollViewerHelperFeature ScrollViewerHelperFeature
         {
             get { return (IScrollViewerHelperFeature)GetValue(ScrollViewerHelperFeatureProperty); }
             set { SetValue(ScrollViewerHelperFeatureProperty, value); }
         }
 
-        public static IScrollViewerHelperFeature GetScrollViewerHelperFeatureProperty(DependencyObject obj)
+        /// <summary>
+        /// get scroll viewer helper feature model
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns></returns>
+        public static IScrollViewerHelperFeature GetScrollViewerHelperFeatureProperty(DependencyObject dependencyObject)
         {
-            return (IScrollViewerHelperFeature)obj.GetValue(ScrollViewerHelperFeatureProperty);
+            return (IScrollViewerHelperFeature)dependencyObject.GetValue(ScrollViewerHelperFeatureProperty);
         }
 
-        public static void SetScrollViewerHelperFeatureProperty(DependencyObject obj, IScrollViewerHelperFeature value)
+        /// <summary>
+        /// set scroll viewer helper feature model
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value">value</param>
+        public static void SetScrollViewerHelperFeatureProperty(DependencyObject dependencyObject, IScrollViewerHelperFeature value)
         {
-            obj.SetValue(ScrollViewerHelperFeatureProperty, value);
+            dependencyObject.SetValue(ScrollViewerHelperFeatureProperty, value);
         }
 
+        /// <summary>
+        /// scroll viewer helper feature model
+        /// </summary>
         public static readonly DependencyProperty ScrollViewerHelperFeatureProperty =
             DependencyProperty.Register("ScrollViewerHelperFeature", typeof(IScrollViewerHelperFeature), typeof(ScrollViewerHelperFeatureBehavior), new PropertyMetadata(null));
 
+        #endregion
 
+        /// <inheritdoc/>
         protected override void OnAttached()
         {
             ScrollViewerHelperFeature.ScrollViewer = AssociatedObject;
             AssociatedObject.ScrollChanged += AssociatedObject_ScrollChanged;
-            //AssociatedObject.SizeChanged += AssociatedObject_SizeChanged;
         }
 
-        //static int _count = 0;
-        private void AssociatedObject_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            //Log.Debug($"ScrollViewer size changed ({_count++})");
-        }
-
-        private void AssociatedObject_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        /// <summary>
+        /// scroll viewer scroll changed event handler
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="eventArgs">event args</param>
+        private void AssociatedObject_ScrollChanged(object sender, ScrollChangedEventArgs eventArgs)
         {
             var m = ScrollViewerHelperFeature;
-            if (e.HorizontalChange != 0)
-                m.HorizontalOffset = e.HorizontalOffset;
-            if (e.VerticalChange != 0)
-                m.VerticalOffset = e.VerticalOffset;
+            if (eventArgs.HorizontalChange != 0)
+                m.HorizontalOffset = eventArgs.HorizontalOffset;
+            if (eventArgs.VerticalChange != 0)
+                m.VerticalOffset = eventArgs.VerticalOffset;
         }
 
+        /// <inheritdoc/>
         protected override void OnDetaching()
         {
             AssociatedObject.ScrollChanged -= AssociatedObject_ScrollChanged;
