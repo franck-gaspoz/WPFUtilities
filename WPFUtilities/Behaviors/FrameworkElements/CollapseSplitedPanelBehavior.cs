fecef766119e316f81eb2c6cfd39da76bb4c19a2
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-03 07:28:39 +0100

    fix behavior

--- WPFUtilities/Behaviors/FrameworkElements/CollapseSplitedPanelBehavior.cs
+++ WPFUtilities/Behaviors/FrameworkElements/CollapseSplitedPanelBehavior.cs
@@ -8,28 +8,41 @@ using WPFUtilities.Helpers;
 namespace WPFUtilities.Behaviors.FrameworkElements
 {
     /// <summary>
-    /// parameters: IsCollapsed , target
+    /// collapse a split panel behavior
     /// </summary>
     public class CollapseSplitedPanelBehavior
         : Behavior<FrameworkElement>
     {
-        #region props
+        #region properties
+
+        /// <summary>
+        /// panel default length
+        /// </summary>
         public double DefaultLength
         {
             get => (double)GetValue(DefaultLengthProperty);
             set { SetValue(DefaultLengthProperty, value); }
         }
 
-        public static double GetDefaultLength(DependencyObject obj)
-        {
-            return (double)obj.GetValue(DefaultLengthProperty);
-        }
+        /// <summary>
+        /// get panel default length
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>default length</returns>
+        public static double GetDefaultLength(DependencyObject dependencyObject)
+            => (double)dependencyObject.GetValue(DefaultLengthProperty);
 
-        public static void SetDefaultLength(DependencyObject obj, double value)
-        {
-            obj.SetValue(DefaultLengthProperty, value);
-        }
+        /// <summary>
+        /// set panel default length
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value">vakue</param>
+        public static void SetDefaultLength(DependencyObject dependencyObject, double value)
+            => dependencyObject.SetValue(DefaultLengthProperty, value);
 
+        /// <summary>
+        /// default length property
+        /// </summary>
         public static readonly DependencyProperty DefaultLengthProperty =
             DependencyProperty.RegisterAttached(
                 "DefaultLength",
@@ -37,22 +50,34 @@ namespace WPFUtilities.Behaviors.FrameworkElements
                 typeof(CollapseSplitedPanelBehavior),
                 new PropertyMetadata(80d));
 
+        /// <summary>
+        /// panel length
+        /// </summary>
         public double Length
         {
             get => (double)GetValue(LengthProperty);
             set { SetValue(LengthProperty, value); }
         }
 
-        public static double GetLength(DependencyObject obj)
-        {
-            return (double)obj.GetValue(LengthProperty);
-        }
+        /// <summary>
+        /// get panel length
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>length</returns>
+        public static double GetLength(DependencyObject dependencyObject)
+            => (double)dependencyObject.GetValue(LengthProperty);
 
-        public static void SetLength(DependencyObject obj, double value)
-        {
-            obj.SetValue(LengthProperty, value);
-        }
+        /// <summary>
+        /// set length
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value">value</param>
+        public static void SetLength(DependencyObject dependencyObject, double value)
+            => dependencyObject.SetValue(LengthProperty, value);
 
+        /// <summary>
+        /// length property
+        /// </summary>
         public static readonly DependencyProperty LengthProperty =
             DependencyProperty.RegisterAttached(
                 "Length",
@@ -60,22 +85,34 @@ namespace WPFUtilities.Behaviors.FrameworkElements
                 typeof(CollapseSplitedPanelBehavior),
                 new PropertyMetadata(80d));
 
+        /// <summary>
+        /// row
+        /// </summary>
         public int Row
         {
             get => (int)GetValue(RowProperty);
             set { SetValue(RowProperty, value); }
         }
 
-        public static int GetRow(DependencyObject obj)
-        {
-            return (int)obj.GetValue(RowProperty);
-        }
+        /// <summary>
+        /// get row
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>row</returns>
+        public static int GetRow(DependencyObject dependencyObject)
+            => (int)dependencyObject.GetValue(RowProperty);
 
-        public static void SetRow(DependencyObject obj, int value)
-        {
-            obj.SetValue(RowProperty, value);
-        }
+        /// <summary>
+        /// set row
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value">value</param>
+        public static void SetRow(DependencyObject dependencyObject, int value)
+            => dependencyObject.SetValue(RowProperty, value);
 
+        /// <summary>
+        /// row property
+        /// </summary>
         public static readonly DependencyProperty RowProperty =
             DependencyProperty.RegisterAttached(
                 "Row",
@@ -83,22 +120,34 @@ namespace WPFUtilities.Behaviors.FrameworkElements
                 typeof(CollapseSplitedPanelBehavior),
                 new PropertyMetadata(-1));
 
+        /// <summary>
+        /// column
+        /// </summary>
         public int Col
         {
             get => (int)GetValue(ColProperty);
             set { SetValue(ColProperty, value); }
         }
 
-        public static int GetCol(DependencyObject obj)
-        {
-            return (int)obj.GetValue(ColProperty);
-        }
+        /// <summary>
+        /// get column
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>column</returns>
+        public static int GetCol(DependencyObject dependencyObject)
+            => (int)dependencyObject.GetValue(ColProperty);
 
-        public static void SetCol(DependencyObject obj, int value)
-        {
-            obj.SetValue(ColProperty, value);
-        }
+        /// <summary>
+        /// set column
+        /// </summary>
+        /// <param name="dependencyObject">dependencyObject</param>
+        /// <param name="value">value</param>
+        public static void SetCol(DependencyObject dependencyObject, int value)
+            => dependencyObject.SetValue(ColProperty, value);
 
+        /// <summary>
+        /// column property
+        /// </summary>
         public static readonly DependencyProperty ColProperty =
             DependencyProperty.RegisterAttached(
                 "Col",
@@ -106,22 +155,35 @@ namespace WPFUtilities.Behaviors.FrameworkElements
                 typeof(CollapseSplitedPanelBehavior),
                 new PropertyMetadata(-1));
 
+        /// <summary>
+        /// is collapsed
+        /// </summary>
         public bool IsCollapsed
         {
             get => (bool)GetValue(IsCollapsedProperty);
             set { SetValue(IsCollapsedProperty, value); }
         }
 
-        public static bool GetIsCollapsed(DependencyObject obj)
-        {
-            return (bool)obj.GetValue(IsCollapsedProperty);
-        }
+        /// <summary>
+        /// get is collapsed
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <returns>is collapsed</returns>
 
-        public static void SetIsCollapsed(DependencyObject obj, bool value)
-        {
-            obj.SetValue(IsCollapsedProperty, value);
-        }
+        public static bool GetIsCollapsed(DependencyObject dependencyObject)
+            => (bool)dependencyObject.GetValue(IsCollapsedProperty);
 
+        /// <summary>
+        /// set is collapsed
+        /// </summary>
+        /// <param name="dependencyObject">dependency Object</param>
+        /// <param name="value">value</param>
+        public static void SetIsCollapsed(DependencyObject dependencyObject, bool value)
+            => dependencyObject.SetValue(IsCollapsedProperty, value);
+
+        /// <summary>
+        /// is collapsed
+        /// </summary>
         public static readonly DependencyProperty IsCollapsedProperty =
             DependencyProperty.RegisterAttached(
                 "IsCollapsed",
@@ -131,8 +193,14 @@ namespace WPFUtilities.Behaviors.FrameworkElements
 
         #endregion
 
+        /// <summary>
+        /// is associated
+        /// </summary>
         public bool IsAssociated { get; protected set; }
 
+        /// <summary>
+        /// cache is initialized
+        /// </summary>
         public bool CacheInitialized { get; protected set; }
 
         GridLength _length;
@@ -145,9 +213,11 @@ namespace WPFUtilities.Behaviors.FrameworkElements
         int _col;
         bool _applyOnCol => _col > -1;
 
+        /// <inheritdoc/>
         protected override void OnAttached()
             => InitTracking(AssociatedObject);
 
+        /// <inheritdoc/>
         protected override void OnDetaching()
             => EndTracking(AssociatedObject);
 
@@ -203,13 +273,13 @@ namespace WPFUtilities.Behaviors.FrameworkElements
             DependencyObject d,
             DependencyPropertyChangedEventArgs e)
         {
-            if (
-                d != null && e.NewValue is bool isCollapsed
+            if (d != null && e.NewValue is bool isCollapsed
                 && d is CollapseSplitedPanelBehavior obj
                 && obj.IsAssociated)
                 obj.CollapsedChanged(isCollapsed);
         }
-        public void CollapsedChanged(bool isCollapsed)
+
+        void CollapsedChanged(bool isCollapsed)
         {
             if (_applyOnRow)
             {
