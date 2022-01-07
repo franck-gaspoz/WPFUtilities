6f44e4eed3548e900225d4c7f7dfd3995349dba4
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-07 07:00:41 +0100

    add sample logging control

---
+++ SampleApp/Components/Logging/ILogViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+using WPFUtilities.ComponentModels;
+
+namespace SampleApp.Components.Logging
+{
+    /// <summary>
+    /// a log view model
+    /// </summary>
+    public interface ILogViewModel : IModelBase
+    {
+        /// <summary>
+        /// messages
+        /// </summary>
+        BindingList<string> Messages { get; }
+    }
+}
---
+++ SampleApp/Components/Logging/LogViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+using WPFUtilities.ComponentModels;
+
+namespace SampleApp.Components.Logging
+{
+    public class LogViewModel : ModelBase, IModelBase, ILogViewModel
+    {
+        /// <inheritdoc/>
+        public BindingList<string> Messages { get; protected set; }
+            = new BindingList<string>();
+
+        /// <summary>
+        /// creates a new instance
+        /// </summary>
+        public LogViewModel()
+        {
+
+        }
+    }
+}
