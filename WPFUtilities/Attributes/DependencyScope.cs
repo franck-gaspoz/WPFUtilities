7db42fe39d22490f636d69be63f0ab2faa9ac3c2
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-07 06:37:26 +0100

    add auto registration class attribute

---
+++ WPFUtilities/Attributes/DependencyScope.cs
@@ -0,0 +1,23 @@
+﻿namespace WPFUtilities.Attributes
+{
+    /// <summary>
+    /// dependency scope
+    /// </summary>
+    public enum DependencyScope
+    {
+        /// <summary>
+        /// singleton
+        /// </summary>
+        Singleton,
+
+        /// <summary>
+        /// transient
+        /// </summary>
+        Transient,
+
+        /// <summary>
+        /// scoped
+        /// </summary>
+        Scoped
+    }
+}
---
+++ WPFUtilities/Attributes/DependencyServiceAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WPFUtilities.Attributes
+{
+    /// <summary>
+    /// dependency service attributes: allow a class to be auto registered in dependency injector
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class DependencyServiceAttribute : Attribute
+    {
+        /// <summary>
+        /// dependency scope
+        /// </summary>
+        public DependencyScope DependencyScope { get; private set; }
+
+        /// <summary>
+        /// register a singleton dependency for interface {IClassName} and class {ClassName}
+        /// </summary>
+        public DependencyServiceAttribute()
+        {
+            DependencyScope = DependencyScope.Singleton;
+        }
+
+        /// <summary>
+        /// register dependecy for specified scope and for interface {IClassName} and class {ClassName}
+        /// </summary>
+        /// <param name="dependencyScope">dependency scope</param>
+        public DependencyServiceAttribute(DependencyScope dependencyScope)
+        {
+            DependencyScope = dependencyScope;
+        }
+    }
+}
