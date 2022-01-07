14724751607102cd07c546e7daf473c79043a9f6
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-07 06:27:21 +0100

    fix doc

--- WPFUtilities/Attributes/DependencyServicesAttribute.cs
+++ WPFUtilities/Attributes/DependencyServicesAttribute.cs
@@ -3,7 +3,7 @@
 namespace WPFUtilities.Attributes
 {
     /// <summary>
-    /// hosted services attributes: allow component to be scaned for auto registering types in dependency injector
+    /// dependency services attributes: allow an assembly to be scaned for auto registering types in dependency injector
     /// </summary>
     [AttributeUsage(AttributeTargets.Assembly)]
     public class DependencyServicesAttribute : Attribute
