dff28834f59a6d4b93f927e193d1374eca5a1cff
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-07 08:49:56 +0100

    improve sample

--- SampleApp/Commands/OnMainWindowShownCommand.cs
+++ SampleApp/Commands/OnMainWindowShownCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows.Input;
 
+using Microsoft.Extensions.Logging;
+
 using WPFUtilities.Commands;
 
 namespace SampleApp.Commands
@@ -9,6 +11,17 @@ namespace SampleApp.Commands
     /// </summary>
     public class OnMainWindowShownCommand : AbstractCommand<OnMainWindowShownCommand>, ICommand
     {
+        ILogger<OnMainWindowShownCommand> _logger;
+
+        /// <summary>
+        /// creates a new instance
+        /// </summary>
+        /// <param name="logger">logger</param>
+        public OnMainWindowShownCommand(ILogger<OnMainWindowShownCommand> logger)
+        {
+            _logger = logger;
+        }
+
         /// <inheritdoc/>
         public override bool CanExecute(object parameter)
             => true;
@@ -19,7 +32,7 @@ namespace SampleApp.Commands
         /// <param name="parameter">not used</param>
         public override void Execute(object parameter)
         {
-            // ...
+            _logger.LogInformation("* main window is shown *");
         }
     }
 }
