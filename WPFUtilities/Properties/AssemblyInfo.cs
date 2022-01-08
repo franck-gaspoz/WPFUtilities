d159c80ac944ef78b42d14a3f7e21caf53e8cf30
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-08 05:47:04 +0100

    version update

--- WPFUtilities/Properties/AssemblyInfo.cs
+++ WPFUtilities/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using WPFUtilities.Attributes;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: ServiceDependencies]
