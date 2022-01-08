cc5c7f34639ea01c228374686a2a184d18da7355
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-08 05:10:20 +0100

    avoid log in log view model

--- SampleApp/Components/Logging/LogViewModel.cs
+++ SampleApp/Components/Logging/LogViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using System.ComponentModel;
 
-using Microsoft.Extensions.Logging;
-
 using WPFUtilities.Attributes;
 using WPFUtilities.ComponentModels;
 
@@ -16,15 +14,5 @@ namespace SampleApp.Components.Logging
         /// <inheritdoc/>
         public BindingList<string> Messages { get; protected set; }
             = new BindingList<string>();
-
-        /// <summary>
-        /// creates a new instance
-        /// </summary>
-        public LogViewModel(
-            ILogger<LogViewModel> logger
-            )
-        {
-            //logger.LogDebug($"{this.GetType().Name} created");
-        }
     }
 }
