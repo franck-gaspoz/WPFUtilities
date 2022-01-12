9af104db810a4ff68b6a2ea722245fe70413f233
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-12 12:09:31 +0100

    simplify usage demo

--- SampleApp/App.xaml.cs
+++ SampleApp/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System.Windows;
-
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
+﻿
 using SampleApp.Components.UI;
 
 using WPFUtilities.Components.Appl;
@@ -26,48 +22,6 @@ namespace SampleApp
                 MainWindowType = typeof(MainWindow),
                 MainWindowComponentType = typeof(MainWindowComponent)
             };
-        }
-
-        /// <summary>
-        /// initialize application [step 1]
-        /// </summary>
-        protected override void Initialize()
-        {
-            base.Initialize();
-        }
-
-        /// <summary>
-        /// configure component [step 2]
-        /// </summary>
-        /// <param name="services">services</param>
-        public override void Configure(HostBuilderContext context, IServiceCollection services)
-        {
-            base.Configure(context, services);
-        }
-
-        /// <summary>
-        /// initialize main window [step 5]
-        /// </summary>
-        /// <param name="mainWindow">main window</param>
-        protected override void InitializeMainWindow(Window mainWindow)
-        {
-            base.InitializeMainWindow(mainWindow);
-        }
-
-        /// <summary>
-        /// on start ui
-        /// </summary>
-        protected override void OnStartUI()
-        {
-            base.OnStartUI();
-        }
-
-        /// <summary>
-        /// on shtudown
-        /// </summary>
-        protected override void OnShutdown()
-        {
-
         }
     }
 }
