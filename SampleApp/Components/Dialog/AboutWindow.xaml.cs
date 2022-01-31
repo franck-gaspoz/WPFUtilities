80192f32b01731debaa502c2bb370320576fee56
franck gaspoz <franck.gaspoz@gmail.com>
2022-01-31 12:25:06 +0100

    add dialog window

---
+++ SampleApp/Components/Dialog/AboutWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SampleApp.Components.Dialog
+{
+    /// <summary>
+    /// AboutWindow controller
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
