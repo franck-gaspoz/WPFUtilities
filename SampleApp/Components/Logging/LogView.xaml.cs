ddb2c01829029af267c02dcc1e249a5eedb50640
franck gaspoz <franck.gaspoz@gmail.com>
2022-02-20 03:43:19 +0100

    cleanup

--- SampleApp/Components/Logging/LogView.xaml.cs
+++ SampleApp/Components/Logging/LogView.xaml.cs
@@ -14,7 +14,6 @@ namespace SampleApp.Components.Logging
             set { SetValue(TitleProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TitleProperty =
             DependencyProperty.Register("Title", typeof(string), typeof(LogView), new PropertyMetadata("LogView title"));
 
