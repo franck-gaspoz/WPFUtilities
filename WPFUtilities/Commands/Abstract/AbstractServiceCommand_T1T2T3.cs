b68c1daa1df7d59b058a9ca4a7bbe03168129b21
franck gaspoz <franck.gaspoz@gmail.com>
2022-02-26 19:17:29 +0100

    add T1T2T3 service command

---
+++ WPFUtilities/Commands/Abstract/AbstractServiceCommand_T1T2T3.cs
@@ -0,0 +1,39 @@
+﻿
+using WPFUtilities.Components.ServiceComponent;
+using WPFUtilities.Components.Services.Command;
+
+namespace WPFUtilities.Commands.Abstract
+{
+    /// <summary>
+    /// command that requires service context
+    /// </summary>
+    public abstract class AbstractServiceCommand
+        <ImplType, TParam1, TParam2, TParam3> :
+            AbstractServiceCommand<ImplType>,
+            IServiceCommand<TParam1, TParam2, TParam3>
+            where ImplType : IServiceCommand<TParam1, TParam2, TParam3>
+    {
+        /// <summary>
+        /// creates a new instance
+        /// </summary>
+        /// <param name="serviceProvider">service component provider</param>
+        public AbstractServiceCommand(IServiceComponentProvider serviceProvider)
+            : base(serviceProvider) { }
+
+        /// <inheritdoc/>
+        public override void Execute(IServiceCommandExecuteContext context, object parameter)
+        {
+            var array = ToValidParameterArray(parameter, 3);
+
+            Execute(
+                context,
+                TransformParameter<TParam1>(0, array),
+                TransformParameter<TParam2>(1, array),
+                TransformParameter<TParam3>(2, array)
+                );
+        }
+
+        /// <inheritdoc/>
+        public abstract void Execute(IServiceCommandExecuteContext context, TParam1 param1, TParam2 param2, TParam3 param3);
+    }
+}
