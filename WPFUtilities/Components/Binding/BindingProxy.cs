f0a2fe55eee7192a174922e5467ecff0a8a44513
franck gaspoz <franck.gaspoz@gmail.com>
2022-02-27 17:32:36 +0100

    added binding proxy

---
+++ WPFUtilities/Components/Binding/BindingProxy.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace WPFUtilities.Components.Binding
+{
+    /// <summary>
+    /// binding proxy
+    /// </summary>
+    public class BindingProxy : Freezable
+    {
+        #region Overrides of Freezable
+
+        /// <inheritdoc/>
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Data
+        /// </summary>
+        public object Data
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        /// <summary>
+        /// data property
+        /// </summary>
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register(
+                "Data",
+                typeof(object),
+                typeof(BindingProxy),
+                new UIPropertyMetadata(null));
+    }
+}
