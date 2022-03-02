b79de06e7e14bf7684f9653edb23451001ae6acd
franck gaspoz <franck.gaspoz@gmail.com>
2022-03-02 23:32:30 +0100

    add error view

---
+++ SampleApp/Components/UI/ErrorView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SampleApp.Components.UI
+{
+    /// <summary>
+    /// Logique d'interaction pour ErrorView.xaml
+    /// </summary>
+    public partial class ErrorView : UserControl
+    {
+        public ErrorView()
+        {
+            InitializeComponent();
+        }
+
+        public string Error
+        {
+            get { return (string)GetValue(ErrorProperty); }
+            set { SetValue(ErrorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ErrorProperty =
+            DependencyProperty.Register(
+                "Error",
+                typeof(string),
+                typeof(ErrorView),
+                new PropertyMetadata(null));
+    }
+}
